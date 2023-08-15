f3ee166e9384e33cb6b6e4c68f83a065115f9919
Titilope Apuabi <tyaps002@gmail.com>
2023-08-15 23:17:27 +0100

    Connect to database

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const adminSchema = new mongoose.Schema({
-    staffNo: string,
+    staffno: string,
     name: string,
     email: string,
     password: string
