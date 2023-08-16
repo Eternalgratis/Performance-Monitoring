2e5a6adc1bacac70c1d33a4f720b03f35bd13392
Titilope Apuabi <tyaps002@gmail.com>
2023-08-17 00:46:59 +0100

    Add basic styling

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import'bootstrap/dist/css/bootstrap.min.css'
 
 
 function Homepage () {
@@ -8,6 +9,8 @@ function Homepage () {
     return (
         <div>
             <h1>Welcome! {user}</h1>
+            <a href="/admin" className="btn btn-primary active m-2" role="button">Signup as Admin</a>
+            <a href="/accountofficer" className="btn btn-info active m-2" role="button">Signup as Account Officer</a>
         </div>
     )
 }
