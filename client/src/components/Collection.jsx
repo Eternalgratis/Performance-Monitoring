8e6a8330404dc2eb26b7c67942036bbfd3bb91f6
Titilope Apuabi <tyaps002@gmail.com>
2023-09-13 21:05:15 +0100

    Add excel workbook

--- client/src/components/Collection.jsx
+++ client/src/components/Collection.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useState } from "react";
+import { utils, read } from "xlsx"
 
 function RevenueCollection () {
+    const file_type = []
+    const handleChange = (e) => {
+        const selected_file = e.target.file[0]
+    }
     return (
-        <h1>This is where the collections will be</h1>
+        <input type="file" onChange={handleChange} />
     )
 }
 
