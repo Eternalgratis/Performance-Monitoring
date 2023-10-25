9781bdba165e31b0a6e767cd6d146a88ea4fbc63
Titilope Apuabi <tyaps002@gmail.com>
2023-10-25 23:13:17 +0100

    Add dependencies

--- client/src/components/Collection.jsx
+++ client/src/components/Collection.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import { utils, read } from "xlsx"
 
 function RevenueCollection () {
-    const file_type = []
+    const [data, setData] = useState([])
+
+    const addRow = () => {
+        setData([...data, Array(data[0].length).fill('')])
+    }
+
     const handleChange = (e) => {
         const selected_file = e.target.file[0]
     }
