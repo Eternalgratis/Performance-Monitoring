6cc04fa0fa42b941c9c22e1e7406da7332284124
Titilope Apuabi <tyaps002@gmail.com>
2023-11-15 13:06:52 +0100

    Updated collection page

--- client/src/components/Collection.jsx
+++ client/src/components/Collection.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useState } from "react";
-import { utils, read } from "xlsx"
 
 function RevenueCollection () {
-    const file_type = []
     const handleChange = (e) => {
         const selected_file = e.target.file[0]
     }
+
     return (
         <input type="file" onChange={handleChange} />
     )
